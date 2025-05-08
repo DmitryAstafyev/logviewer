98dadf3de2e38f707b353f7d3fc32067c344e169
DmitryAstafyev <dvastafyev@gmail.com>
2025-05-08 18:03:28 +0200

    Parser/Source API refactoring (step 1)

--- application/apps/indexer/parsers/src/dlt/mod.rs
+++ application/apps/indexer/parsers/src/dlt/mod.rs
@@ -2,10 +2,7 @@ pub mod attachment;
 pub mod fmt;
 mod options;
 
-use crate::{
-    dlt::fmt::FormattableMessage, parse_all, someip::FibexMetadata as FibexSomeipMetadata, Error,
-    LogMessage, ParseYield, Parser,
-};
+use crate::{dlt::fmt::FormattableMessage, someip::FibexMetadata as FibexSomeipMetadata};
 use byteorder::{BigEndian, WriteBytesExt};
 use dlt_core::{
     dlt,
@@ -17,14 +14,15 @@ pub use dlt_core::{
     filtering::{DltFilterConfig, ProcessedDltFilterConfig},
 };
 use serde::Serialize;
-use std::{io::Write, ops::Range};
+use std::{io::Write, ops::Range, sync::Arc};
 
 use self::{attachment::FtScanner, fmt::FormatOptions};
+use definitions::*;
 
 /// The most likely minimal bytes count needed to parse a DLT message.
 const MIN_MSG_LEN: usize = 20;
 
-impl LogMessage for FormattableMessage<'_> {
+impl LogMessage for FormattableMessage {
     fn to_writer<W: Write>(&self, writer: &mut W) -> Result<usize, std::io::Error> {
         let bytes = self.message.as_bytes();
         let len = bytes.len();
@@ -72,13 +70,13 @@ impl LogMessage for RawMessage {
 }
 
 #[derive(Default)]
-pub struct DltParser<'m> {
+pub struct DltParser {
     pub filter_config: Option<ProcessedDltFilterConfig>,
-    pub fibex_dlt_metadata: Option<&'m FibexDltMetadata>,
-    pub fmt_options: Option<&'m FormatOptions>,
+    pub fibex_dlt_metadata: Option<Arc<FibexDltMetadata>>,
+    pub fmt_options: Option<Arc<FormatOptions>>,
     pub with_storage_header: bool,
     ft_scanner: FtScanner,
-    fibex_someip_metadata: Option<&'m FibexSomeipMetadata>,
+    fibex_someip_metadata: Option<Arc<FibexSomeipMetadata>>,
     offset: usize,
 }
 
@@ -102,8 +100,8 @@ impl DltRawParser {
         &mut self,
         input: &[u8],
         _timestamp: Option<u64>,
-    ) -> Result<(usize, Option<ParseYield<RawMessage>>), Error> {
-        let (rest, consumed) = dlt_consume_msg(input)?;
+    ) -> Result<(usize, Option<ParseYield<RawMessage>>), ParserError> {
+        let (rest, consumed) = dlt_consume_msg(input).map_err(map_dlt_err)?;
         let msg = consumed.map(|c| RawMessage {
             content: Vec::from(&input[0..c as usize]),
         });
@@ -123,8 +121,8 @@ impl DltRangeParser {
         &mut self,
         input: &[u8],
         _timestamp: Option<u64>,
-    ) -> Result<(usize, Option<ParseYield<RangeMessage>>), Error> {
-        let (rest, consumed) = dlt_consume_msg(input)?;
+    ) -> Result<(usize, Option<ParseYield<RangeMessage>>), ParserError> {
+        let (rest, consumed) = dlt_consume_msg(input).map_err(map_dlt_err)?;
         let msg = consumed.map(|c| {
             self.offset += c as usize;
             RangeMessage {
@@ -141,21 +139,21 @@ impl DltRangeParser {
     }
 }
 
-impl<'m> DltParser<'m> {
+impl DltParser {
     pub fn new(
         filter_config: Option<ProcessedDltFilterConfig>,
-        fibex_dlt_metadata: Option<&'m FibexDltMetadata>,
-        fmt_options: Option<&'m FormatOptions>,
-        fibex_someip_metadata: Option<&'m FibexSomeipMetadata>,
+        fibex_dlt_metadata: Option<FibexDltMetadata>,
+        fmt_options: Option<FormatOptions>,
+        fibex_someip_metadata: Option<FibexSomeipMetadata>,
         with_storage_header: bool,
     ) -> Self {
         Self {
             filter_config,
-            fibex_dlt_metadata,
+            fibex_dlt_metadata: fibex_dlt_metadata.map(|md| Arc::new(md)),
             with_storage_header,
-            fmt_options,
+            fmt_options: fmt_options.map(|fo| Arc::new(fo)),
             ft_scanner: FtScanner::new(),
-            fibex_someip_metadata,
+            fibex_someip_metadata: fibex_someip_metadata.map(|md| Arc::new(md)),
             offset: 0,
         }
     }
@@ -164,15 +162,17 @@ impl<'m> DltParser<'m> {
         &mut self,
         input: &[u8],
         timestamp: Option<u64>,
-    ) -> Result<(usize, Option<ParseYield<FormattableMessage<'m>>>), Error> {
-        match dlt_message(input, self.filter_config.as_ref(), self.with_storage_header)? {
+    ) -> Result<(usize, Option<ParseYield<FormattableMessage>>), ParserError> {
+        match dlt_message(input, self.filter_config.as_ref(), self.with_storage_header)
+            .map_err(map_dlt_err)?
+        {
             (rest, dlt_core::parse::ParsedMessage::FilteredOut(_n)) => {
                 let consumed = input.len() - rest.len();
                 self.offset += consumed;
                 Ok((consumed, None))
             }
             (_, dlt_core::parse::ParsedMessage::Invalid) => {
-                Err(Error::Parse("Invalid parse".to_owned()))
+                Err(ParserError::Parse("Invalid parse".to_owned()))
             }
             (rest, dlt_core::parse::ParsedMessage::Item(i)) => {
                 let attachment = self.ft_scanner.process(&i);
@@ -184,9 +184,9 @@ impl<'m> DltParser<'m> {
 
                 let msg = FormattableMessage {
                     message: msg_with_storage_header,
-                    fibex_dlt_metadata: self.fibex_dlt_metadata,
-                    options: self.fmt_options,
-                    fibex_someip_metadata: self.fibex_someip_metadata,
+                    fibex_dlt_metadata: self.fibex_dlt_metadata.clone(),
+                    options: self.fmt_options.clone(),
+                    fibex_someip_metadata: self.fibex_someip_metadata.clone(),
                 };
                 let consumed = input.len() - rest.len();
                 self.offset += consumed;
@@ -205,24 +205,21 @@ impl<'m> DltParser<'m> {
     }
 }
 
-impl From<DltParseError> for Error {
-    fn from(value: DltParseError) -> Self {
-        match value {
-            DltParseError::Unrecoverable(e) | DltParseError::ParsingHickup(e) => {
-                Error::Parse(e.to_string())
-            }
-            DltParseError::IncompleteParse { needed: _ } => Error::Incomplete,
+fn map_dlt_err(err: DltParseError) -> ParserError {
+    match err {
+        DltParseError::Unrecoverable(e) | DltParseError::ParsingHickup(e) => {
+            ParserError::Parse(e.to_string())
         }
+        DltParseError::IncompleteParse { needed: _ } => ParserError::Incomplete,
     }
 }
 
-impl<'m> Parser<FormattableMessage<'m>> for DltParser<'m> {
+impl Parser<FormattableMessage> for DltParser {
     fn parse(
         &mut self,
         input: &[u8],
         timestamp: Option<u64>,
-    ) -> Result<impl Iterator<Item = (usize, Option<ParseYield<FormattableMessage<'m>>>)>, Error>
-    {
+    ) -> Result<Vec<(usize, Option<ParseYield<FormattableMessage>>)>, ParserError> {
         parse_all(input, timestamp, MIN_MSG_LEN, |input, timestamp| {
             self.parse_item(input, timestamp)
         })
@@ -234,7 +231,7 @@ impl Parser<RangeMessage> for DltRangeParser {
         &mut self,
         input: &[u8],
         timestamp: Option<u64>,
-    ) -> Result<impl Iterator<Item = (usize, Option<ParseYield<RangeMessage>>)>, Error> {
+    ) -> Result<Vec<(usize, Option<ParseYield<RangeMessage>>)>, ParserError> {
         parse_all(input, timestamp, MIN_MSG_LEN, |input, timestamp| {
             self.parse_item(input, timestamp)
         })
@@ -246,7 +243,7 @@ impl Parser<RawMessage> for DltRawParser {
         &mut self,
         input: &[u8],
         timestamp: Option<u64>,
-    ) -> Result<impl Iterator<Item = (usize, Option<ParseYield<RawMessage>>)>, Error> {
+    ) -> Result<Vec<(usize, Option<ParseYield<RawMessage>>)>, ParserError> {
         parse_all(input, timestamp, MIN_MSG_LEN, |input, timestamp| {
             self.parse_item(input, timestamp)
         })
